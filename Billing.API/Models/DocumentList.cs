d52f605da2c51b3b32f09bba8a76c12badfd4544
jhoffman-ms <jmh248@gmail.com>
2022-02-04 09:22:47 -0300

    fix: fix issue when get invoices from sap

--- Billing.API/Models/DocumentList.cs
+++ Billing.API/Models/DocumentList.cs
@@ -8,6 +8,11 @@ namespace Billing.API.Models
 
         [JsonProperty(PropertyName = "odata.metadata")]
         public string metadata { get; set; }
+        [JsonProperty(PropertyName = "odata.count")]
+        public string count { get; set; }
+        
+        [JsonProperty(PropertyName = "odata.nextLink")]
+        public string nextLink { get; set; }
         public List<DocumentItem> value { get; set; }
     }
 }
